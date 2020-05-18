c5b4b9a4131f9949687cdbd4f00abb31fdc2d942
Sabbir <sabbir106706@gmail.com>
2020-05-19 05:51:07 +0600

    s1-A using cpp

---
+++ Sheet #1 (Data type - Conditions)/A. Say Hello With C++.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    string S;
+    cin>>S;
+    cout<<"Hello, "<<S;
+    return 0;
+}
+
+
