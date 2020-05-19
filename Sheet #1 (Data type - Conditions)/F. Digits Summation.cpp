b1da40db8b1acdef2218f9d4de3c61cd203e2c34
Sabbir <sabbir106706@gmail.com>
2020-05-19 14:12:10 +0600

    s1-F using cpp

---
+++ Sheet #1 (Data type - Conditions)/F. Digits Summation.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long n,m;
+    int a,b;
+    cin>>n>>m;
+    a=(int)(n%10);
+    b=(int)(m%10);
+    cout<<a+b;
+    return 0;
+}
+
+
