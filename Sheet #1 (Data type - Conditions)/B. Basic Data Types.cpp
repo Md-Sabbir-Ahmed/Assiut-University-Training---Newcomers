fca78dedcc1326ca9e532bc1f6171cfe42f3af76
Sabbir <sabbir106706@gmail.com>
2020-05-19 14:04:14 +0600

    s1-B using cpp

---
+++ Sheet #1 (Data type - Conditions)/B. Basic Data Types.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long b;
+    double e;
+    char c;
+    int a;
+    float d;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<"\n";
+    cout<<b<<"\n";
+    cout<<c<<"\n";
+    cout<<fixed<<setprecision(2)<<d<<"\n";
+    cout<<fixed<<setprecision(1)<<e<<"\n";
+    return 0;
+}
+
+
