6419ed0468fb7cc2a5acf4219ca1b58d47eba207
Sabbir <sabbir106706@gmail.com>
2020-05-19 14:06:02 +0600

    s1-C using cpp

---
+++ Sheet #1 (Data type - Conditions)/C. Simple Calculator.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    long long x,y;
+    cin>>x>>y;
+    cout<<x<<" + "<<y<<" = "<<x+y<<"\n";
+    cout<<x<<" * "<<y<<" = "<<x*y<<"\n";
+    cout<<x<<" - "<<y<<" = "<<x-y<<"\n";
+
+    return 0;
+}
+
+
