4850157457839d34d5a5d67873456219e491013d
Sabbir <sabbir106706@gmail.com>
2020-05-19 14:13:34 +0600

    s1-I using cpp

---
+++ Sheet #1 (Data type - Conditions)/I. Welcome for you with Conditions.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int a,b;
+    cin>>a>>b;
+    if(a>=b)
+    {
+        cout<<"Yes"<<"\n";
+    }
+    else{
+        cout<<"No"<<"\n";
+    }
+    return 0;
+}
+
+
