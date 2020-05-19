8a46846449f601332d3bd54c31e5af6ac895cf96
Sabbir <sabbir106706@gmail.com>
2020-05-19 14:10:35 +0600

    s1-E using cpp

---
+++ Sheet #1 (Data type - Conditions)/E. Area of a Circle.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pi 3.141592653
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    double r;
+    cin>>r;
+    double area=pi*r*r;
+    cout<<fixed<<setprecision(9)<<area;
+
+    return 0;
+}
+
+
