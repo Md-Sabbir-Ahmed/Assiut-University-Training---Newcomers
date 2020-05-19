6de2002eae6cb5603578e07f97574e7f7206ed40
Sabbir <sabbir106706@gmail.com>
2020-05-19 14:16:43 +0600

    s1-L using cpp

---
+++ Sheet #1 (Data type - Conditions)/L. The Brothers.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    string f1,s1,f2,s2;
+    cin>>f1>>s1;
+    cin>>f2>>s2;
+    if(s1==s2)
+    {
+        cout<<"ARE Brothers";
+    }
+    else{
+        cout<<"NOT";
+    }
+    return 0;
+}
+
+
