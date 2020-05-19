fee514a2e5a9ab47918ebd5a9c29502a4d877ad9
Sabbir <sabbir106706@gmail.com>
2020-05-20 05:40:53 +0600

    s1-Q using cpp

---
+++ Sheet #1 (Data type - Conditions)/Q. Coordinates of a Point.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    double x,y;
+    cin>>x>>y;
+    if(x==0 && y==0)
+    {
+        cout<<"Origem";
+    }
+    else if(x!=0 && y==0)
+    {
+        cout<<"Eixo X";
+    }
+    else if(y!=0 && x==0)
+    {
+        cout<<"Eixo Y";
+    }
+    else if(x>0 && y>0)
+    {
+        cout<<"Q1";
+    }
+    else if(x<0 && y>0)
+    {
+        cout<<"Q2";
+    }
+    else if(x<0 && y<0)
+    {
+        cout<<"Q3";
+    }
+    else if(x>0 && y<0)
+    {
+        cout<<"Q4";
+    }
+
+    return 0;
+}
+
+
