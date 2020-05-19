91c1b36ab63b0ab8df296006a5cd640083acca53
Sabbir <sabbir106706@gmail.com>
2020-05-19 14:08:59 +0600

    s1-D using cpp

---
+++ Sheet #1 (Data type - Conditions)/D. Difference.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long A,B,C,D,X;
+    cin>>A>>B>>C>>D;
+    X=(A*B)-(C*D);
+    cout<<"Difference = "<<X<<"\n";
+    return 0;
+}
+
+
