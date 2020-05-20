5bffc631d6d7cba440cdf299050be099287860ac
Sabbir <sabbir106706@gmail.com>
2020-05-20 17:02:11 +0600

    s2-F using cpp

---
+++ Sheet #2 (Loops)/F. Multiplication table.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin>>n;
+    for(int i=1;i<=12;++i)
+    {
+        cout<<n<<" * "<<i<<" = "<<i*n<<"\n";
+    }
+    return 0;
+}
+
+
