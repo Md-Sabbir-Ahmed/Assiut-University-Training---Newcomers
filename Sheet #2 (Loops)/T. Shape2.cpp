2af743396fc527d6dd61f741da3406a78e4d342a
Sabbir <sabbir106706@gmail.com>
2020-05-20 23:18:46 +0600

    s2-T using cpp

---
+++ Sheet #2 (Loops)/T. Shape2.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int k=1;k<=n-i;k++)
+        {
+            cout<<" ";
+        }
+        for(int j=1;j<=(2*i)-1;j++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
+
+
