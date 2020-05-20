cbf2579c10c44be636df4e4b5c931693ff060fc6
Sabbir <sabbir106706@gmail.com>
2020-05-20 12:47:08 +0600

    s1-G using cpp

---
+++ Sheet #1 (Data type - Conditions)/G. Summation from 1 to N.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long n;
+    cin>>n;
+    cout<<n*(n+1)/2;
+    return 0;
+}
+
+
