33a8b14cd8ce5ca5ea8206fe76181fac37cd0e61
Sabbir <sabbir106706@gmail.com>
2020-05-20 16:51:51 +0600

    s2-A using cpp

---
+++ Sheet #2 (Loops)/A. 1 to N.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;++i)
+    {
+        cout<<i<<"\n";
+    }
+    return 0;
+}
+
+
