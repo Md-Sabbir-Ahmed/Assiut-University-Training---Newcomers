c9adc47c13b50bc71921ac2d41b51281722678fa
Sabbir <sabbir106706@gmail.com>
2020-06-18 03:18:08 +0600

    s2-N using cpp

---
+++ Sheet #2 (Loops)/N. Numbers Histogram.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    string s;
+    cin>>s;
+    int t,a;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a;
+        for(int i=1;i<=a;i++)
+        {
+            cout<<s;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
+
+
